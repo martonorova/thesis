38028238ba88eb4bfcf71882f9c0710fe0c0bae9
Marton Orova <morova@rapidminer.com>
2019-11-08 15:02:43 +0100

    Implement timeseries handling in proxy

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -4,10 +4,12 @@ from getpass import getpass
 import json
 import time
 import logging
+import re
 
 app = Flask(__name__)
 
 server_host = 'http://rapidminer-server:8080'
+
 
 @app.route('/')
 def hello_world():
@@ -24,78 +26,50 @@ def query():
     print("Query Data")
     print(request.data)
 
-    # WARNING this only extracts the first target, it disregards the others, if provided
-    target = request.json['targets'][0]['target']
+    request_json = request.json
+    result = []
 
-    response = requests.get(server_host + '/api/rest/process/{}'.format(target),
-                       auth=('admin', 'changeit'))
+    # Check what the selected metric type is ('timeserie' or 'table') for each target in the request query
+    for target in request_json['targets']:
+        target_type = target['type']
+        target_name = target['target']
+        # response from the RM Webservice containing the values for the given target
+        response = requests.get(server_host + '/api/rest/process/{}'.format(target_name),
+                                    auth=('admin', 'changeit'))
+        json_data = json.loads(response.text)
 
-    json_data = json.loads(response.text)
-    
-    # print("JSON DATA: {}".format(json_data))
+        if target_type == 'timeserie':
+            print("TIMESERIES")
+            target_result = convert_to_series(json_data, target_name)
+            result.append(target_result)
 
-    columns = []
-    rows = []
+        elif target_type == 'table':
+            target_result = convert_to_table(json_data)
+            result.append(target_result)
 
-    # this is good for many rows
-    if type(json_data) == list:
-        
-        for column in json_data[0]:
-
-            column_obj = {
-                "text": column,
-                "type": get_type(json_data[0][column])
-            }
-
-            columns.append(column_obj)
-
-        for row in json_data:
-            new_row = []
-            for column in columns:
-                new_row.append(row[column['text']])
-            rows.append(new_row)
-
-    elif type(json_data) == dict:
-        
-        row = []
-
-        for field in json_data:
-            column_obj = {
-                "text": field,
-                "type": get_type(json_data[field])
-            }
-
-            columns.append(column_obj)
-            row.append(json_data[field])
-        
-        rows.append(row)
-
-    result = [
-        {
-            "columns": columns,
-            "rows": rows,
-            "type": 'table'
-        }
-    ]
+        else:
+            raise ValueError('Unknown target type: {}'.format(target_type))
 
     print('Result:')
     print(json.dumps(result))
     return json.dumps(result)
 
+
 @app.route('/search', methods=['POST'])
 def search():
 
     response = requests.get(server_host + '/api/rest/service/list',
-                       auth=('admin', 'changeit'))
+                            auth=('admin', 'changeit'))
 
     webservices_json_list = json.loads(response.text)
-    
+
     webservice_names = []
 
     for webservice in webservices_json_list:
         webservice_names.append(webservice['name'])
 
     return json.dumps(webservice_names)
+
 
 @app.route('/annotations', methods=['POST'])
 def annotations():
@@ -121,10 +95,10 @@ def parameters():
         # get the list of webservices and get the parameters of the one with the name provided in the query
         # TODO refactor this, duplicate of /search endpoint
         response = requests.get(server_host + '/api/rest/service/list',
-                       auth=('admin', 'changeit'))
+                                auth=('admin', 'changeit'))
 
         webservices_json_list = json.loads(response.text)
-        
+
         webservice_params = []
 
         for webservice in webservices_json_list:
@@ -142,6 +116,71 @@ def get_type(value):
         return 'number'
     else:
         return 'string'
+
+# converts data from RM wevservice (a single target) to table format
+
+
+def convert_to_table(json_data):
+    columns = []
+    rows = []
+
+    # this is good for many rows
+    if type(json_data) == list:
+        for column in json_data[0]:
+            column_obj = {
+                "text": column,
+                "type": get_type(json_data[0][column])
+            }
+            columns.append(column_obj)
+
+        for row in json_data:
+            new_row = []
+            for column in columns:
+                new_row.append(row[column['text']])
+            rows.append(new_row)
+    # this is good for only one row from the webservice
+    elif type(json_data) == dict:
+        row = []
+        for field in json_data:
+            column_obj = {
+                "text": field,
+                "type": get_type(json_data[field])
+            }
+            columns.append(column_obj)
+            row.append(json_data[field])
+
+        rows.append(row)
+
+    result = {
+        "columns": columns,
+        "rows": rows,
+        "type": 'table'
+    }
+
+    return result
+
+def convert_to_series(json_data, target_name):
+    datapoints = []
+
+    # this is good for many rows
+    if type(json_data) == list:
+        r = re.compile("^((?!badword).)*$")
+        value_name = list(filter(r.match, list(json_data[0].keys())))[0]
+        for row in json_data:
+            datapoints.append([
+                row[value_name],
+                row['timestamp']
+            ])
+
+    else:
+        raise ValueError("Provided timeseries is not a list!")
+
+
+    result = {
+        "target": target_name,
+        "datapoints": datapoints
+    }
+    return result
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
