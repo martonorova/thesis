3bd0683e9851f31415655cc6e386b4f92c94c206
Marton Orova <marton.orova@gmail.com>
2019-11-19 00:55:12 +0100

    Start implementing business logic in python-datasource

--- python-datasource/python-datasource/datasource.py
+++ python-datasource/python-datasource/datasource.py
@@ -2,9 +2,14 @@ from flask import Flask, request
 from flask_api import FlaskAPI, status, exceptions
 from flask_cors import CORS
 import requests
+import mysql.connector
+from mysql.connector import Error
+import json
 
 app = Flask(__name__)
 CORS(app)
+
+weather_api_host = 'http://weather-api:7000'
 
 table = {
     'columns': [
@@ -24,12 +29,74 @@ def test_datasource():
 def search():
     pass
 
+@app.route('/query')
+def query():
+    return "QUERY"
+    
+
+def get_all_time_records():
+    response = requests.get(weather_api_host + '/all')
+
+    json_data = json.loads(response.text)
+
+    return json_data
+
 
 def create_table_data():
-    pass
 
+    delta = 0.2
+
+    all_time_records = get_all_time_records()
+    new_data = read_data_from_db()
+
+    for record in all_time_records:
+        
+
+
+# https://pynative.com/python-mysql-select-query-to-fetch-data/
 def read_data_from_db():
-    pass
+    try:
+        connection = mysql.connector.connect(
+            host='mysql',
+            database='weather',
+            user='weatheruser',
+            password='userpass'
+        )
+
+        sql_select_records_query = "SELECT * FROM newyork"
+        cursor = connection.cursor()
+        cursor.execute(sql_select_records_query)
+        records = cursor.fetchall()
+
+        # print(type(records))
+        # print(records)
+
+        data = []
+
+        for row in records:
+            record = {
+                'date': row[0],
+                'day_of_the_year': int(row[1]),
+                'max_temp': float(row[2]),
+                'min_temp': float(row[3])
+            }
+            data.append(record)
+        
+        # print(data)
+        # print(type(data[0]['date']))
+        # print(type(int(data[0]['day_of_the_year'])))
+        # print(type(data[0]['max_temp']))
+        # print(type(data[0]['min_temp']))
+
+        return data
+
+    except Error as error:
+        print("Error reading data from MySQL ", error)
+    finally:
+        if (connection.is_connected()):
+            connection.close()
+            #cursor.close()
+            print("MySQL connection closed")
 
 
 
