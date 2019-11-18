7280935572961f1173529798bcbbf525ac77661b
Marton Orova <morova@rapidminer.com>
2019-11-18 16:02:49 +0100

    Prepare New York data and prepopulate MySQL db with it

--- mysql/scripts/02_create_newyork_table.sql
+++ mysql/scripts/02_create_newyork_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE newyork (
-    -- id INT NOT NULL AUTO_INCREMENT,
-    time DECIMAL(15),
-    temperature DECIMAL(10, 4),
-    PRIMARY KEY (time)
+    date DATE,
+    day_of_the_year DECIMAL(3, 0),
+    max_daily_temp DECIMAL(10, 4),
+    min_daily_temp DECIMAL(10, 4),
+    PRIMARY KEY (date)
 );
